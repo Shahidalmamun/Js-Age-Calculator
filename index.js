f1b8b8756e8dd430565eb6829b000dc99b2cf1d1
Shahidalmamun <shahidalmamun789@gmail.com>
2025-07-19 18:42:28 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//Age calculator 
+
+let birthYear = prompt("Enter your birth year:");
+let currentYear = 2025;
+
+let age = currentYear - birthYear;
+
+console.log(`you are ${age} years old. If you were born in ${birthYear}.`);
+
+
+
